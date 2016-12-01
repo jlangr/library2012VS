b97500823747194ae029d27c30e9eaea3f1e752e
Jeff Langr <jeff@langrsoft.com>
2016-12-01 15:51:28 -0800

    name normalizer exercise ready

--- libraryTest/NameNormalizerTest.cpp
+++ libraryTest/NameNormalizerTest.cpp
@@ -5,51 +5,48 @@
 using namespace testing;
 using namespace std;
 
-// You should probably start with StringUtil::split. 
-// Unfortunately it only supports a single delimiter currently.
-
-TEST(ANameNormalizer, ReturnsAnEmptyStringWhenGivenSame)
+TEST(ANameNormalizer, DISABLED_ReturnsAnEmptyStringWhenGivenSame)
 {
     ASSERT_THAT(NormalizedName{""}.AsString(), Eq(""));
 }
 
-TEST(ANameNormalizer, SwapsFirstAndLastNames)
+TEST(ANameNormalizer, DISABLED_SwapsFirstAndLastNames)
 {
     ASSERT_THAT(NormalizedName{"Joseph Heller"}.AsString(), Eq("Heller, Joseph"));
     ASSERT_THAT(NormalizedName{"Haruki Murakami"}.AsString(), Eq("Murakami, Haruki"));
 }
 
-TEST(ANameNormalizer, SimplyReturnsASingleWordName)
+TEST(ANameNormalizer, DISABLED_SimplyReturnsASingleWordName)
 {
     ASSERT_THAT(NormalizedName{"Plato"}.AsString(), Eq("Plato"));
 }
 
-TEST(ANameNormalizer, TrimsWhitespace)
+TEST(ANameNormalizer, DISABLED_TrimsWhitespace)
 {
     ASSERT_THAT(NormalizedName{"  Big Boi   "}.AsString(), Eq("Boi, Big"));
 }
 
-TEST(ANameNormalizer, ReplacesMiddleNameWithInitial)
+TEST(ANameNormalizer, DISABLED_ReplacesMiddleNameWithInitial)
 {
     ASSERT_THAT(NormalizedName{"Henry David Thoreau"}.AsString(), Eq("Thoreau, Henry D."));
 }
 
-TEST(ANameNormalizer, DoesNotInitializeOneLetterMiddleName)
+TEST(ANameNormalizer, DISABLED_DoesNotInitializeOneLetterMiddleName)
 {
     ASSERT_THAT(NormalizedName{"Harry S Truman"}.AsString(), Eq("Truman, Harry S"));
 }
 
-TEST(ANameNormalizer, InitializesEachOfMultipleMiddleNames)
+TEST(ANameNormalizer, DISABLED_InitializesEachOfMultipleMiddleNames)
 {
     ASSERT_THAT(NormalizedName{"Julia Scarlett Elizabeth Louis-Dreyfus"}.AsString(), Eq("Louis-Dreyfus, Julia S. E."));
 }
 
-TEST(ANameNormalizer, AppendsSuffixesToEnd)
+TEST(ANameNormalizer, DISABLED_AppendsSuffixesToEnd)
 {
     ASSERT_THAT(NormalizedName{"Martin Luther King, Jr."}.AsString(), Eq("King, Martin L., Jr."));
 }
 
-TEST(ANameNormalizer, ThrowsWhenNameContainsTwoCommas)
+TEST(ANameNormalizer, DISABLED_ThrowsWhenNameContainsTwoCommas)
 {
-    //ASSERT_THROW(NormalizedName{"Thurston, Howell, III"), InvalidNameException);
+    ASSERT_THROW(NormalizedName{"Thurston, Howell, III"}, InvalidNameException);
 }
