5b970751a6f4931044583a65db9c4cf619226679
Jeff Langr <jeff@langrsoft.com>
2016-12-08 18:54:26 -0800

    added video store

---
+++ library/Movie.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include "PriceCode.h"
+
+class Movie
+{
+public:
+	explicit Movie(const std::string& title, int priceCode);
+	virtual ~Movie();
+	std::string title() const;
+	int priceCode() const;
+   void setPriceCode(int priceCode);
+
+private:
+	std::string mTitle;
+	int mPriceCode;
+};
